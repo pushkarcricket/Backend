cfba0fb41407fdd521e0d57b227f0045b98ddbd6
Pushkar <pushkarcricket@gmail.com>
2024-10-03 18:57:11 +0530

    types of api

---
+++ Api/index.js
@@ -0,0 +1,28 @@
+// fetch("http://wherethesis.at/V1/satellites/25544")
+// .then((response)=>{
+//     console.log("data is coming",response)
+
+// }).catch((err)=>{
+//     console.log("unable to fetch",err)
+// })
+
+// let run="hello world";
+// console.log(run)
+
+//Api with endpoint and query Parameter
+
+// fetch("https://bored-api.appbrewery.com/filter?type=social & participants=2")
+// .then((Response)=>{
+//     console.log("the data is coming",Response)
+// }).catch((err)=>{
+//     console.log("something is wrong ", err)
+// })
+
+//Path Parameter
+// fetch("https://bored-api.appbrewery.com/activity/3943506")
+// .then((res)=>{
+//     console.log("this is respose", res)
+// }).catch((err)=>{
+//     console.log("error while loading the api",err)
+// })
+
