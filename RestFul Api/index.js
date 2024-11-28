59a71cf61ec19b2680bfce7157c760066aeefa42
Pushkar <pushkarcricket@gmail.com>
2024-11-28 15:27:36 +0530

    28Nov

--- RestFul Api/index.js
+++ RestFul Api/index.js
@@ -33,13 +33,35 @@ app.route("/api/users/:id")
 })
 
 .put((req,res)=>{
-    res.send("pending")
+
+    const first_name =req.params.first_name
+
+    const user= users.find(user=> user.first_name === first_name)
+    // if(!user){res.send(404).send("the course you are looking is not exit")}
+        user.first_name=req.body.first_name
+        res.send(user)
+    console.log(user)
+    
 })
 .patch((req,res)=>{
+
+    
     res.send("your data is editing this thing")
 })
-.delete((req,res)=>{
-    res.send("your id is deleted ")
+app.delete((req,res)=>{
+    const id= Number(req.params.id)
+
+
+    // const deletedUser=users.filter(user=> user.id !== req.params.id)
+    // users=deletedUser;
+    // res.send(deletedUser)
+    // console.log(deletedUser)
+
+    const userIndex=users.findIndex(user=> user.id === id);
+    if(userIndex === -1){
+        return res.status(404).send("User is not found")
+    }
+    const deletedUser= users.splice(userIndex, 1)
 })
 
 
@@ -48,23 +70,34 @@ app.route("/api/users/:id")
 
  app.post("/api/users/:id", (req,res)=>{
      const body=req.body;
-     console.log("body", body)
+    //  console.log("body", body)
 
-     users.push({...body, id: users.length +1})
-     fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err)=>{
-        if(err){
-            console.log("Error writing to file",err)
-        }else{
-            return  res.json({satus: "success", message: "User added successfully"})
+    //  users.push({...body, id: users.length +1})
+    //  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err)=>{
+    //     if(err){
+    //         console.log("Error writing to file",err)
+    //     }else{
+    //         return  res.json({satus: "success", message: "User added successfully"})
 
-        }
+    //     }
 
        
         
-     })
+    //  })
+
+    const user={
+        id: users.length+1,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        gender: req.body.gender,
+        job_title: req.body.job_title
+    }
+    users.push(user)
+    res.send(user)
     
  })
 
 // app.patch("/api/users/:id"(req))
 
-app.listen(4060)
+app.listen(4070)
